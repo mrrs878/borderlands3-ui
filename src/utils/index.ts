05727210a23bf806ff133b1a97b1c65b8ba87678
mrrs878 <mrrs878@foxmail.com>
2021-10-26 21:22:41 +0800

    perf: 完善可交互按钮（InteractiveButton）

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-10-13 21:05:34
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-10-22 16:40:55
+ * @LastEditTime: 2021-10-26 14:06:22
  * @FilePath: \borderlands3-ui\src\utils\index.ts
  */
 type Classes = Record<string, Function | boolean>;
@@ -13,11 +13,34 @@ export function className(...args) {
   if (args.length === 1 && typeof args[0] === 'object') {
     const keys = Reflect.ownKeys(args[0]);
     for (let i = 0; i < keys.length; i += 1) {
-      console.log(args[0][keys[i]]());
       if (args[0][keys[i]]()) return keys[i];
     }
     return '';
   }
   const [name, when, name2 = ''] = args;
   return when ? name : name2;
+}
+
+// https://gist.github.com/danielpquinn/dd966af424030d47e476
+/**
+ * Get path data for a rounded rectangle. Allows for different radius on each corner.
+ * @param  {Number} w   Width of rounded rectangle
+ * @param  {Number} h   Height of rounded rectangle
+ * @param  {Number} tlr Top left corner radius
+ * @param  {Number} trr Top right corner radius
+ * @param  {Number} brr Bottom right corner radius
+ * @param  {Number} blr Bottom left corner radius
+ * @return {String}     Rounded rectangle SVG path data
+ */
+export function roundedRectData(w: number, h: number, tlr: number, trr: number,
+  brr: number, blr: number) {
+  return `M 0 ${tlr
+  } A ${tlr} ${tlr} 0 0 1 ${tlr} 0`
+  + ` L ${w - trr} 0`
+  + ` A ${trr} ${trr} 0 0 1 ${w} ${trr
+  } L ${w} ${h - brr
+  } A ${brr} ${brr} 0 0 1 ${w - brr} ${h
+  } L ${blr} ${h
+  } A ${blr} ${blr} 0 0 1 0 ${h - blr
+  } Z`;
 }
