426854858289a08955cd62e7a258c89621f786f5
mrrs878 <mrrs878@foxmail.com>
2021-10-28 21:39:36 +0800

    perf: 完善LeveledUp;添加MissionComplete

--- src/Message/Message.stories.tsx
+++ src/Message/Message.stories.tsx
@@ -2,7 +2,7 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-10-26 21:59:58
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-10-27 20:46:28
+ * @LastEditTime: 2021-10-28 21:14:35
  * @FilePath: \borderlands3-ui\src\Message\Message.stories.tsx
  */
 // Generated with util/create-component.js
@@ -14,4 +14,6 @@ export default {
   component: Message,
 };
 
-export const LeveledUp = () => <Message.LeveledUp />;
+export const LeveledUp = () => <Message.LeveledUp level={75} />;
+
+export const MissionComplete = () => <Message.MissionComplete missionName="Children of the Vault" />;
--- src/Message/index.tsx
+++ src/Message/index.tsx
@@ -2,40 +2,90 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-10-26 21:59:58
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-10-27 22:02:54
+ * @LastEditTime: 2021-10-28 21:24:57
  */
 import React, { FC, useEffect } from 'react';
 import { SVG } from '@svgdotjs/svg.js';
 import style from './index.module.less';
 
-interface IMessageProps {}
+interface ILeveledUpProps {
+  level: number
+}
 
-const LeveledUp: FC<IMessageProps> = () => {
+const LeveledUp: FC<ILeveledUpProps> = (props) => {
   useEffect(() => {
     const svgContainer: HTMLDivElement = document.querySelector('#progressSVG');
     if (svgContainer) {
       const width = 300;
-      const height = 30;
+      const height = 50;
       const canvas = SVG().addTo(svgContainer)
-        .size(width, height * 6);
+        .size(width, width);
 
       const gradient = canvas.gradient('linear', (add) => {
-        add.stop(0, '#ffae00', 1);
-        add.stop(width, 'transparent', 1);
+        add.stop(0, 'rgba(0, 0, 0, 0)', 1);
+        add.stop(0.5, '#f9d730', 1);
+        add.stop(1, 'rgba(0, 0, 0, 0)', 1);
+        add.from(0, 0);
+        add.to(1, 0);
       });
       for (let i = 0; i < 6; i += 1) {
         canvas.path(`
-        M 0 0
-        L ${width / 2} ${height / 2}
-        L ${width} 0
-        L ${width} ${height}
-        L ${width / 2} ${height / 2}
-        L 0 ${height}
-        Z
-      `).attr({ fill: gradient }).transform({ rotate: i * 30, translateY: height });
+          M 0 0
+          L ${width / 2} ${height / 2}
+          L ${width} 0
+          L ${width} ${height}
+          L ${width / 2} ${height / 2}
+          L 0 ${height}
+          Z
+        `).attr({ fill: gradient, filter: 'blur(1px)' })
+          .transform({ rotate: i * 30, translateY: width / 2 })
+          .stroke('rgba(0, 0, 0, 0)');
       }
+
+      canvas.text(`LEVEL ${props.level}`)
+        .addClass(style.levelText)
+        .center(width / 2, width / 2)
+        .attr({ fill: '#f9d730', filter: 'drop-shadow(0 0 6px #824111)' });
+
+      canvas.text('YOU LEVELED UP!')
+        .addClass(style.leveledUpText)
+        .center(width / 2, width / 2 + 60)
+        .attr({ fill: '#f9d730', filter: 'drop-shadow(0 0 6px #824111)' });
     }
-  }, []);
+  }, [props.level]);
+
+  return (
+    <div className={style.container}>
+      <div className={style.svgContainer} id="progressSVG" />
+    </div>
+  );
+};
+
+interface IMissionCompleteProps {
+  missionName: string
+}
+
+const MissionComplete: FC<IMissionCompleteProps> = (props) => {
+  useEffect(() => {
+    const svgContainer: HTMLDivElement = document.querySelector('#progressSVG');
+    if (svgContainer) {
+      const width = 600;
+      const height = 300;
+      const canvas = SVG().size(width, height).addTo(svgContainer);
+
+      const t = canvas.text('MISSION COMPLETE')
+        .addClass(style.levelText)
+        .center(width / 2, height / 2)
+        .attr({ fill: '#f9d730', filter: 'drop-shadow(0 0 6px #824111)' });
+
+      console.log(t.width());
+
+      canvas.text(props.missionName)
+        .addClass(style.leveledUpText)
+        .attr({ fill: '#fff', filter: 'drop-shadow(0 0 6px #000)' })
+        .center(width / 2, height / 2 + 60);
+    }
+  }, [props.missionName]);
 
   return (
     <div className={style.container}>
@@ -46,6 +96,7 @@ const LeveledUp: FC<IMessageProps> = () => {
 
 const Message = {
   LeveledUp,
+  MissionComplete,
 };
 
 export { Message };
