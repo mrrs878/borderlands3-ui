a3050fba55a6b8ef1ac3c5f7cd633eda5b080b87
mrrs878 <mrrs878@foxmail.com>
2021-11-02 22:06:25 +0800

    perf: 完善Card.Gun

--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -2,11 +2,25 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-11-01 21:00:34
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-11-01 22:04:57
+ * @LastEditTime: 2021-11-02 21:49:21
  */
 import React, { useEffect, useRef } from 'react';
 import { Svg, SVG } from '@svgdotjs/svg.js';
 import style from './index.module.less';
+
+interface IGunIndicatorProps {
+  title: string;
+  value: string;
+}
+
+const GunIndicator = (props: IGunIndicatorProps) => (
+  <div className={style.gunIndicatorContainer}>
+    {
+      props.value && <span className={style.gunIndicatorContainerValue}>{ props.value }</span>
+    }
+    <span className={style.gunIndicatorContainerTitle}>{ props.title }</span>
+  </div>
+);
 
 const Gun = () => {
   const backgroundSVGRef = useRef<Svg>(null);
@@ -20,9 +34,9 @@ const Gun = () => {
 
   useEffect(() => {
     if (backgroundSVGRef.current) {
-      const clientWidth = 300;
-      const clientHeight = 200;
-      const offset1 = 15;
+      const clientWidth = 400;
+      const clientHeight = 300;
+      const offset1 = 20;
       backgroundSVGRef.current.polygon([
         [offset1 / 2, 0],
         [clientWidth - offset1, 0],
@@ -69,6 +83,31 @@ const Gun = () => {
   return (
     <div className={style.container}>
       <div className={style.background} id="backgroundSVGRef" />
+      <div className={style.header}>
+        <div className={style.score}>101</div>
+        <span>ITEM SCORE</span>
+        <div className={style.blank} />
+        <span>LVL REQ.</span>
+        <div className={style.level}>4</div>
+      </div>
+      <div className={style.title}>xxx</div>
+      <div className={style.content}>
+        <div className={style.left}>
+          <GunIndicator title="DOMAGE" value="24" />
+          <GunIndicator title="ACCURACY" value="50%" />
+          <GunIndicator title="HANDLING" value="56%" />
+          <GunIndicator title="RELOAD TILE" value="1.5S" />
+          <GunIndicator title="FIRE RATE" value="11.5S" />
+          <GunIndicator title="MAGAZINE SIZE" value="5" />
+        </div>
+        <div className={style.right}>
+          <div className={style.rarity}>
+            UNCOMMON
+          </div>
+          <div className={style.blank} />
+          <div className={style.rarityBottom} />
+        </div>
+      </div>
     </div>
   );
 };
