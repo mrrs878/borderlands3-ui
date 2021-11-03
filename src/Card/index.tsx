e70fa30fef8f53858b850772bfc088429b1b0cb6
mrrs878 <mrrs878@foxmail.com>
2021-11-03 21:58:39 +0800

    perf: 完善Card.Gun

--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -2,7 +2,7 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-11-01 21:00:34
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-11-02 21:49:21
+ * @LastEditTime: 2021-11-03 21:49:36
  */
 import React, { useEffect, useRef } from 'react';
 import { Svg, SVG } from '@svgdotjs/svg.js';
@@ -35,8 +35,9 @@ const Gun = () => {
   useEffect(() => {
     if (backgroundSVGRef.current) {
       const clientWidth = 400;
-      const clientHeight = 300;
+      const clientHeight = 320;
       const offset1 = 20;
+      const storkWidth = 4;
       backgroundSVGRef.current.polygon([
         [offset1 / 2, 0],
         [clientWidth - offset1, 0],
@@ -53,8 +54,21 @@ const Gun = () => {
         [offset1 / 2, 0],
       ]).fill('#21252d').stroke({
         color: '#117b22',
-        width: 2,
+        width: storkWidth,
       });
+
+      backgroundSVGRef.current.polygon([
+        [clientWidth - storkWidth, clientHeight - offset1 * 3.2],
+        [clientWidth - storkWidth, clientHeight - offset1 - storkWidth],
+        [clientWidth - offset1, clientHeight - storkWidth],
+        [clientWidth / 2 + (offset1 + offset1 * 3), clientHeight - storkWidth],
+        [clientWidth / 2 + offset1 * 3, clientHeight - offset1 - storkWidth],
+        [clientWidth / 2 - offset1 * 3, clientHeight - offset1 - storkWidth],
+        [clientWidth / 2 - (offset1 + offset1 * 3), clientHeight - storkWidth],
+        [offset1, clientHeight - storkWidth],
+        [storkWidth, clientHeight - offset1 - storkWidth],
+        [storkWidth, clientHeight - offset1 * 3.2],
+      ]).fill('#1c1e22');
 
       backgroundSVGRef.current.polygon([
         [offset1 / 2, 0],
@@ -96,18 +110,38 @@ const Gun = () => {
           <GunIndicator title="DOMAGE" value="24" />
           <GunIndicator title="ACCURACY" value="50%" />
           <GunIndicator title="HANDLING" value="56%" />
-          <GunIndicator title="RELOAD TILE" value="1.5S" />
-          <GunIndicator title="FIRE RATE" value="11.5S" />
+          <GunIndicator title="RELOAD TILE" value="1.5s" />
+          <GunIndicator title="FIRE RATE" value="11.5s" />
           <GunIndicator title="MAGAZINE SIZE" value="5" />
         </div>
         <div className={style.right}>
           <div className={style.rarity}>
             UNCOMMON
           </div>
-          <div className={style.blank} />
+          <div className={`${style.blank} ${style.gun}`} />
           <div className={style.rarityBottom} />
         </div>
       </div>
+      <div className={style.addition}>
+        <div className={style.additionItem}>
+          <span className={style.additionNumber}>+27%</span>
+          <span className={style.additionDesc}>Weapon Damage</span>
+        </div>
+        <div className={style.additionItem}>
+          <span className={style.additionNumber}>+40%</span>
+          <span className={style.additionDesc}>Melee Damage</span>
+        </div>
+        <div className={style.additionItem}>
+          <span className={style.additionNumber}>2.2x</span>
+          <span className={style.additionDesc}>Weapon Zoom</span>
+        </div>
+        <div className={style.additionItem}>
+          <span className={style.additionDesc}>
+            Critical Hits ricochet 1 bullet at the nearest enemy
+          </span>
+        </div>
+      </div>
+      <div className={style.company} />
     </div>
   );
 };
