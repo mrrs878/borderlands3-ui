26f8956a50895a9b9b1256ad8f1619c0e4891e65
mrrs878 <mrrs878@foxmail.com>
2021-11-05 20:05:18 +0800

    perf: 抽离Card.Gun至单独文件

--- src/Card/Card.stories.tsx
+++ src/Card/Card.stories.tsx
@@ -2,15 +2,39 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-11-01 21:00:34
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-11-01 21:23:02
+ * @LastEditTime: 2021-11-05 19:46:24
  * @FilePath: \borderlands3-ui\src\Card\Card.stories.tsx
  */
 import React from 'react';
-import { Card } from '.';
+import { Card, Company, Rarity } from './index';
 
 export default {
   title: 'Card',
   component: Card,
 };
 
-export const Gun = () => <Card.Gun />;
+export const Gun = () => (
+  <Card.Gun
+    score={101}
+    levelReq={4}
+    title="xxx"
+    damage={24}
+    accuracy={50}
+    handling={56}
+    reloadTime={1.5}
+    fireRate={11.5}
+    magazineSize={5}
+    rarity={Rarity.uncommon}
+    additions={[
+      { desc: 'Weapon Damage', value: '27%' },
+      { desc: 'Melee Damage', value: '40%' },
+      { desc: 'Weapon Zoom', value: '2.2x' },
+    ]}
+    superAddions={[
+      'Critical Hits ricochet 1 bullet at the nearest enemy',
+      'Critical Hits ricochet 1 bullet at the nearest enemy',
+    ]}
+    currency="123"
+    company={Company.jakobs}
+  />
+);
---
+++ src/Card/enum.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: mrrs878@foxmail.com
+ * @Date: 2021-11-05 19:22:45
+ * @LastEditors: mrrs878@foxmail.com
+ * @LastEditTime: 2021-11-05 19:22:46
+ */
+
+export enum Company {
+  jakobs = 'jakobs',
+}
+
+export enum Rarity {
+  uncommon = 'uncommon',
+}
