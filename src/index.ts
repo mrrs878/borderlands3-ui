3a47f46a9be2f564db0acd458ebf3fcb7c88c306
mrrs878 <mrrs878@foxmail.com>
2021-11-14 17:08:22 +0800

    feat: 添加Card、Message组件导出

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-10-12 10:10:23
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-10-22 14:47:31
+ * @LastEditTime: 2021-11-14 17:03:30
  * @FilePath: \borderlands3-ui\src\index.ts
  */
 import './font.less';
@@ -11,3 +11,5 @@ export { Button } from './Button';
 export { Dialog } from './Dialog';
 export { Blood } from './Blood';
 export { Modal } from './Modal';
+export { Card } from './Card';
+export { Message } from './Message';
