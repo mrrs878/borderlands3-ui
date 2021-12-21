dd6dd1dae4b46f82d3d315630d2e5ad78adc5073
mrrs878 <mrrs878@foxmail.com>
2021-12-21 21:53:25 +0800

    feat: 添加单个className判断

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,20 +2,25 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-10-13 21:05:34
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-10-26 14:06:22
+ * @LastEditTime: 2021-12-21 21:51:22
  * @FilePath: \borderlands3-ui\src\utils\index.ts
  */
+
 type Classes = Record<string, Function | boolean>;
 
+export function className(names: string): string;
 export function className(names: Classes): string;
 export function className(name: string, when: boolean, name2?: string): string;
 export function className(...args) {
-  if (args.length === 1 && typeof args[0] === 'object') {
+  if (args.length === 1 && typeof args[0] === 'object' && args[0] !== null) {
     const keys = Reflect.ownKeys(args[0]);
     for (let i = 0; i < keys.length; i += 1) {
       if (args[0][keys[i]]()) return keys[i];
     }
     return '';
+  }
+  if (args.length === 1) {
+    return args[0] ? args[0] : '';
   }
   const [name, when, name2 = ''] = args;
   return when ? name : name2;
