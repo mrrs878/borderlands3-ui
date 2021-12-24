1955e1d5e1d745cf28cdcea6905192a6eb283bfa
mrrs878 <mrrs878@foxmail.com>
2021-12-24 19:54:26 +0800

    fix: 修复缺少的导出

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-10-12 10:10:23
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-12-14 21:13:41
+ * @LastEditTime: 2021-12-24 19:37:49
  * @FilePath: \borderlands3-ui\src\index.ts
  */
 import './font.less';
 
-export { Button } from './Button';
-export { Dialog } from './Dialog';
-export { Blood } from './Blood';
-export { Modal } from './Modal';
-export { Card } from './Card';
-export { Message } from './Message';
-export { Setting } from './Setting';
+export * from './Button';
+export * from './Dialog';
+export * from './Blood';
+export * from './Modal';
+export * from './Card';
+export * from './Message';
+export * from './Setting';
+export * from './Picker';
+export * from './List';
